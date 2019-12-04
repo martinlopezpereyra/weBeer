729a1b1e79b092a5f4d48f90188266a533dad17f
masaalb1 <masaalb1@gmail.com>
2019-12-03 21:19:00 -0300

    Me habia equivocado de nav besis

--- js/header.js
+++ js/header.js
@@ -11,28 +11,6 @@ class nuevoHeader extends HTMLElement {
     <nav id="botones" class="navbar navbar-expand-lg navbar-light bg-light">
       <button id="boton1"type="button" class="btn btn-outline-dark m-2" href="faqs.html" >Sign In</button>
       <button id="boton2" type="button" class="btn btn-outline-dark" href="faqs.html">Register</button>
-    </nav>
-
-  <!-- Segunda NAV-->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <!-- Mi agregado-->
-    <img src="img/we-beer-logo.png" alt="logo shop de cervezas">
-
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Cervezas<span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Promos</a>
-        <a class="nav-item nav-link" href="#">Eventos</a>
-        <a class="nav-item nav-link" href="#">Nosotros</a>
-        <!--<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
-      </div>
-    </div>
-        <ul id="navegar">
-          <li><a href="#"><img class="iconos" src="img/header/icon-faqs.png"></a></li>
-          <li><a href="#"><img class="iconos" src="img/header/icon-shopping-cart.png"></a></li>
-          <li><a href="#"><img class="iconos" src="img/header/icon-beer.png"></a></li>
-          <li><a href="#"><img class="iconos" src="img/header/icon-search.png"></a></li>
-      </ul>
       <!-- Boton Ani-->
 
       <button id="botones"type="button" class="btn btn-outline-dark m-2" data-toggle="modal"
@@ -76,6 +54,30 @@ class nuevoHeader extends HTMLElement {
           </div>
         </div>
       </div>
+
+    </nav>
+
+  <!-- Segunda NAV-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Mi agregado-->
+    <img src="img/we-beer-logo.png" alt="logo shop de cervezas">
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Cervezas<span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Promos</a>
+        <a class="nav-item nav-link" href="#">Eventos</a>
+        <a class="nav-item nav-link" href="#">Nosotros</a>
+        <!--<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
+      </div>
+    </div>
+        <ul id="navegar">
+          <li><a href="#"><img class="iconos" src="img/header/icon-faqs.png"></a></li>
+          <li><a href="#"><img class="iconos" src="img/header/icon-shopping-cart.png"></a></li>
+          <li><a href="#"><img class="iconos" src="img/header/icon-beer.png"></a></li>
+          <li><a href="#"><img class="iconos" src="img/header/icon-search.png"></a></li>
+      </ul>
+
 
   </nav>
 </header>
