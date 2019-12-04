defa7477d32fc7a841d7f3c2a8a4252ba85fc2ec
masaalb1 <masaalb1@gmail.com>
2019-12-03 21:07:44 -0300

    Con boton de contacto

--- js/header.js
+++ js/header.js
@@ -33,6 +33,50 @@ class nuevoHeader extends HTMLElement {
           <li><a href="#"><img class="iconos" src="img/header/icon-beer.png"></a></li>
           <li><a href="#"><img class="iconos" src="img/header/icon-search.png"></a></li>
       </ul>
+      <!-- Boton Ani-->
+
+      <button id="botones"type="button" class="btn btn-outline-dark m-2" data-toggle="modal"
+       data-target="#exampleModal" data-whatever="@getbootstrap">Contact us</button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Consulta</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </div>
+              </button>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Nombre y Apellido</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Usuario</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">E-mail</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Consulta</label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+
+              </form>
+      </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+              <button type="button" class="btn btn-primary">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
   </nav>
 </header>
 `
