a087d7aef31775faec8e7e7bd2098cac27985c62
Sabrina <57275429+masaalb1@users.noreply.github.com>
2020-01-06 22:32:06 -0300

    Version Enero

---
+++ weBeer/js/footer.js
@@ -0,0 +1,92 @@
+
+class nuevoFooter extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML =
+
+`
+<footer class="webintern-footer">
+
+<a name="promosBancarias" id="bancos">
+<img src="img/785X40.jpg" class="promosBancarias" alt="promos bancarias"></a>
+<div class="webintern-footer-inner">
+<div class="container">
+<div class="row">
+
+<div class="col-sm-6 col-md-3 ">
+<img class="logoFooter" src="img/we-beer-logo-footer.png" alt="logo shop de cervezas">
+<div class="simple-text dark padding-sm">
+<p>Somos una tienda especializada en la venta online de cervezas artesanales producidas en Argentina. Nuestro fin es brindarles
+  un catálogo con la mejor cerveza artesanal de todos los rincones de nuestro pais.</p>
+</div>
+<div class="empty-space xs-25 sm-25"></div>
+</div>
+
+<!-- webintern_footer_title -->
+<div class="col-sm-6 col-md-2 footer-2">
+<h4 class="webintern_footer_title h5"><small>SHOP</small></h4>
+<ul class="webintern-footer-list">
+<li><a href="#">Cervezas</a></li>
+<li><a href="#">Promos</a></li>
+<li><a href="#">Eventos</a></li>
+<li><a href="#">Oportunidad!</a></li>
+<li><a href="#">Suma tu producto</a></li>
+<li><a href="#">Registrate</a></li>
+</ul>
+<div class="empty-space xs-25 sm-25"></div>
+</div>
+<div class="col-sm-6 col-md-3 footer-3">
+<!-- webintern_footer_title -->
+<h4 class="webintern_footer_title h5"><small>CONTACTO </small></h4>
+<ul class="webintern-footer-list">
+<li><a href="#">Minorista +54 9 261 5251540</a></li>
+<li><a href="#">Mayorista +54 9 261 5251544</a></li>
+<li><a href="mailto:estefania.idk@gmail.com">ventas@weebeershop.com</a></li>
+<li><a href="mailto:estefania.idk@gmail.com">info@weebeershop.com</a></li>
+<li><a href="https://goo.gl/maps/2rgVdoTgkY6JTNxWA">Rodriguez Peña 200</a></li>
+<li><a href="https://goo.gl/maps/fEN2ZQUnvKwD1ZU46">Mendoza.Argentina</a></li>
+</ul>
+<div class="empty-space xs-25"></div>
+</div>
+<div class="col-xs-12 col-md-4 col-sm-6">
+<div class="marg-sm-b30"></div>
+<!-- webintern_footer_title -->
+<h4 class="webintern_footer_title h5"><small>Subscribite a nuestro Newsletter!!!</small></h4>
+<div class="empty-space marg-lg-b20"></div>
+<!--
+<div class="simple-text last dark ">
+<p>Get latest updates and offers.</p>
+</div>
+-->
+<div class="empty-space marg-lg-b15"></div>
+<!-- TT-SUBSCRIBE -->
+<form method="post">
+  <div class="tt-subscribe">
+    <input type="text" required="" placeholder="Ingresá tu email">
+  <div class="tt-subscribe-submit">
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+    <input type="submit" value="">
+  </div>
+  </div>
+</form>
+  <div class="empty-space marg-lg-b30"></div>
+<!-- TT-SOCAIL -->
+<ul class="tt-socail">
+<li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+<li><a href="https://www.instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+<li><a href="https://twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+
+
+<li><a href="https://www.youtube.com/"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</footer>
+`
+  }
+};
+window.customElements.define('nuevo-Footer', nuevoFooter);
---
+++ weBeer/js/header.js
@@ -0,0 +1,156 @@
+
+class nuevoHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML =
+
+`<header class=" container">
+  <!-- Primera NAV-->
+
+  <div class = "container">
+
+    <nav id="botones" class="navbar navbar-expand-lg navbar-light bg-white p-0">
+
+    <button id="boton1"type="button" class="btn btn-outline-dark m-2" data-toggle="modal" data-target="#exampleModal">Login
+
+    </button>
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <form>
+
+
+    <div class="form-row">
+      <div class="form-group col-md-8">
+        <label for="inputEmail4">Email</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      </div>
+      </div>
+      <div class="form-row">
+      <div class="form-group col-md-8">
+        <label for="inputPassword4">Password</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+      </div>
+      </div>
+      <div class="form-group">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Acordate de mi cuenta!
+          </label>
+        </div>
+      </div>
+
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-light"><a href="registro.html">Registrarme</a></button>
+      <button type="button" class="btn btn-dark">Ingresar</button>
+    </div>
+  </div>
+</div>
+</div>
+<!fin modal------>
+
+      <!-- <button id="boton1"type="button" class="btn btn-outline-dark m-2" href="faqs.html" >Sign In</button>-->
+
+      <button type="button" class="btn btn-outline-dark"><a href="registro.html">Sign Up</a></button>
+
+      <!--<button id="boton2" type="button" class="btn btn-outline-dark" href="faqs.html">Register</button>-->
+
+      <!-- Boton Ani-->
+
+      <!--<button id="botones"type="button" class="btn btn-outline-dark m-2" data-toggle="modal"
+       data-target="#exampleModal" data-whatever="@getbootstrap">Contact us</button>-->
+
+       <!--<button type="button" class="btn btn-dark">Contact</button>-->
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Consulta</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </div>
+            </button>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Nombre y Apellido</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Usuario</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">E-mail</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Consulta</label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+
+              </form>
+      </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+              <button type="button" class="btn btn-primary">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
+    </div>
+
+  <!-- Segunda NAV-->
+
+  <div class = "navDos">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white p-0">
+    <!-- Mi agregado-->
+    <img class="logo" src="img/we-beer-logo.png" alt="logo shop de cervezas">
+    <!---------------->
+    <!--<a class="navbar-brand" href="#">cervezas</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>-->
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="index.html">Cervezas</a>
+        <a class="nav-item nav-link" href="#">Promos</a>
+        <a class="nav-item nav-link" href="#">Eventos</a>
+        <a class="nav-item nav-link" href="#">Nosotros</a>
+        <a class="nav-item nav-link" href="#">Contacto</a>
+        <!--
+        <a class="nav-item nav-link active" href="#">Cervezas<span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
+      </div>
+    </div>
+        <ul id="navegar">
+          <li><a href="faqs.html"><img class="iconos" src="img/header/icon-faqs.png"></a></li>
+          <!--<li><a href="#"><img class="iconos" src="img/header/icon-shopping-cart.png"></a></li>-->
+          <li><a href="shop.html"><img class="iconos" src="img/header/icon-beer.png"></a></li>
+          <li><a href="#"><img class="iconos" src="img/header/icon-search.png"></a></li>
+      </ul>
+  </nav>
+</div>
+</header>
+`
+  }
+};
+window.customElements.define('nuevo-header', nuevoHeader);
